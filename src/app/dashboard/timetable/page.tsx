6eb82b7309c2d3a499415086d556d77e6cc45a4d
Ai.M <Ai.Mokrane@proton.me>
2024-05-31 12:28:18 +0100

    Fixes

--- src/app/dashboard/timetable/page.tsx
+++ src/app/dashboard/timetable/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 /* Utils */
-import { useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import {
+  DateValue,
+  Time,
+  getLocalTimeZone,
+  now,
+} from "@internationalized/date";
 
 /* Components */
 import {
@@ -12,18 +18,70 @@ import {
   SelectItem,
   SelectSection,
   Selection,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+  Input,
+  DatePicker,
+  Divider,
+  Textarea,
+  Tooltip,
 } from "@nextui-org/react";
-import { FiEdit } from "react-icons/fi";
-import { FaCheck } from "react-icons/fa6";
-import { FaXmark } from "react-icons/fa6";
+import { FiPlusCircle } from "react-icons/fi";
+import { CiCalendar } from "react-icons/ci";
+import { CiTimer } from "react-icons/ci";
+import { TbMathFunction } from "react-icons/tb";
+import { SiGoogleclassroom } from "react-icons/si";
+import { HiMiniUserGroup } from "react-icons/hi2";
+import { FaRepeat } from "react-icons/fa6";
+import { CiCircleRemove } from "react-icons/ci";
 
 /* Custom Components */
 import Scheduler from "@/components/UI/Scheduler";
 
-function TimeTable() {
-  const [level, setLevel] = useState<Selection>(new Set(["1hs"]));
-  const [editMode, setEditMode] = useState<boolean>(false);
+/* Types */
+export type Event = {
+  id: string | number;
+  start: string;
+  end: string;
+  calendarId: string;
+  title?: string;
+  description?: string;
+  location?: string;
+  people?: string[];
+  rrule?: string;
+};
 
+function TimeTable() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [error, setError] = useState<boolean>(false);
+  const [removeAll, setRemoveAll] = useState<boolean>(false);
+  const [eventsCount, setEventsCount] = useState<number>(0);
+  const [event, setEvent] = useState<Event>({
+    id: 0,
+    start: "",
+    end: "",
+    calendarId: "school",
+  });
+  const [startDate, setStartDate] = useState<DateValue>(
+    now(getLocalTimeZone()).set({ hour: 8, minute: 0 })
+  );
+  const [endDate, setEndDate] = useState<DateValue>(
+    now(getLocalTimeZone()).set({ hour: 9, minute: 0 })
+  );
+  const [taskData, setTaskData] = useState<Event>({
+    id: "",
+    start: "",
+    end: "",
+    calendarId: "school",
+  });
+  const [level, setLevel] = useState<Selection>(new Set(["1hs"]));
+
+  const minTime = new Time(8);
+  const maxTime = new Time(16);
   const educationLvl = [
     {
       education: "HS",
@@ -61,6 +119,57 @@ function TimeTable() {
     },
   ];
 
+  const formatDate = (date: string): string => {
+    return `${date.substring(0, 10)} ${date.substring(11, 16)}`;
+  };
+
+  const handleInput = (
+    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = event.target;
+    let people: string[] = [];
+
+    if (name === "people") {
+      value.split(",").map((value) => people.push(value));
+    }
+
+    const data = name === "people" ? { [name]: people } : { [name]: value };
+
+    setTaskData((prevTask) => ({
+      ...prevTask,
+      ...data,
+      start: formatDate(startDate.toString()),
+      end: formatDate(endDate.toString()),
+    }));
+  };
+
+  const addTask = () => {
+    const newTask = {
+      ...taskData,
+      id: eventsCount,
+    };
+    setEvent(newTask);
+
+    setTaskData({
+      id: "",
+      start: "",
+      end: "",
+      calendarId: "school",
+    });
+  };
+
+  useEffect(() => {
+    setError(
+      startDate.toDate(getLocalTimeZone()).getHours() < 8 ||
+        startDate.toDate(getLocalTimeZone()).getHours() > 16 ||
+        startDate.compare(endDate) > 0 ||
+        endDate.toDate(getLocalTimeZone()).getHours() < 8 ||
+        endDate.toDate(getLocalTimeZone()).getHours() > 16 ||
+        endDate.compare(startDate) < 0 ||
+        endDate.compare(startDate) == 0
+    );
+  }, [endDate, startDate]);
+
   return (
     <Card fullWidth className="h-full">
       <CardHeader className="w-full">
@@ -68,6 +177,7 @@ function TimeTable() {
           radius="sm"
           selectedKeys={level}
           onSelectionChange={setLevel}
+          aria-label="Select education level"
           className="max-w-28 mr-auto"
         >
           {educationLvl.map((edu, index) => (
@@ -79,43 +189,222 @@ function TimeTable() {
           ))}
         </Select>
 
-        <aside>
-          {!editMode ? (
-            <Button
-              color="primary"
-              radius="sm"
-              variant="shadow"
-              endContent={<FiEdit size={16} />}
-              onClick={() => setEditMode(true)}
-            >
-              Edit
-            </Button>
-          ) : (
-            <div className="flex items-center gap-2">
-              <Button
-                color="danger"
-                radius="sm"
-                variant="ghost"
-                endContent={<FaXmark size={16} />}
-                onClick={() => setEditMode(false)}
-              >
-                Cancel
-              </Button>
-              <Button
-                color="success"
-                variant="solid"
-                radius="sm"
-                endContent={<FaCheck size={16} />}
-                onClick={() => setEditMode(false)}
-              >
-                Save
-              </Button>
-            </div>
-          )}
+        <aside className="flex items-center gap-2">
+          <Button
+            color="danger"
+            radius="sm"
+            variant="ghost"
+            onClick={() => {
+              setRemoveAll(true);
+              setEventsCount(0);
+            }}
+            endContent={<CiCircleRemove size={16} />}
+            aria-label="Remove all study sessions"
+          >
+            Clear
+          </Button>
+
+          <Button
+            color="primary"
+            radius="sm"
+            variant="shadow"
+            endContent={<FiPlusCircle size={16} />}
+            onPress={onOpen}
+            aria-label="Add new study session"
+          >
+            Add
+          </Button>
         </aside>
       </CardHeader>
       <CardBody>
-        <Scheduler level={level.valueOf()} />
+        <Scheduler
+          event={event}
+          removeAll={removeAll}
+          setRemoveAll={setRemoveAll}
+        />
+
+        {/* Add Task */}
+        <Modal
+          hideCloseButton
+          backdrop="blur"
+          size="2xl"
+          isDismissable={false}
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+          aria-labelledby="schedule-study-sessions-modal"
+        >
+          <ModalContent className="p-2">
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex item-center gap-2 m-2">
+                  <CiCalendar size={26} />
+                  <p id="schedule-study-sessions-modal">
+                    Schedule Studying Sessions
+                  </p>
+                </ModalHeader>
+
+                <Divider />
+
+                <ModalBody className="my-4 grid grid-cols-2 gap-6">
+                  <DatePicker
+                    autoFocus
+                    hideTimeZone
+                    isRequired
+                    value={startDate}
+                    isInvalid={
+                      startDate &&
+                      (startDate.toDate(getLocalTimeZone()).getHours() < 8 ||
+                        startDate.toDate(getLocalTimeZone()).getHours() > 16 ||
+                        startDate.compare(endDate) > 0 ||
+                        startDate.compare(endDate) == 0)
+                    }
+                    granularity="minute"
+                    radius="sm"
+                    name="start"
+                    label="Start Date & Time"
+                    hourCycle={24}
+                    timeInputProps={{
+                      startContent: <CiTimer size={24} />,
+                      minValue: minTime,
+                      maxValue: maxTime,
+                    }}
+                    calendarProps={{
+                      weekdayStyle: "short",
+                    }}
+                    onChange={setStartDate}
+                    aria-label="Select start date and time for the study session"
+                  />
+
+                  <DatePicker
+                    hideTimeZone
+                    isRequired
+                    value={endDate}
+                    isInvalid={
+                      endDate &&
+                      (endDate.toDate(getLocalTimeZone()).getHours() < 8 ||
+                        endDate.toDate(getLocalTimeZone()).getHours() > 16 ||
+                        endDate.compare(startDate) < 0 ||
+                        endDate.compare(startDate) == 0)
+                    }
+                    granularity="minute"
+                    name="end"
+                    radius="sm"
+                    label="End Date & Time"
+                    hourCycle={24}
+                    timeInputProps={{
+                      startContent: <CiTimer size={24} />,
+                      minValue: minTime,
+                      maxValue: maxTime,
+                    }}
+                    calendarProps={{
+                      weekdayStyle: "short",
+                    }}
+                    onChange={setEndDate}
+                    aria-label="Select end date and time for the study session"
+                  />
+
+                  <Input
+                    isRequired
+                    type="text"
+                    radius="sm"
+                    name="title"
+                    label="Title"
+                    placeholder="Math"
+                    onChange={handleInput}
+                    endContent={
+                      <TbMathFunction
+                        className="text-2xl text-default-400 pointer-events-none flex-shrink-0"
+                        size={20}
+                      />
+                    }
+                    aria-label="Enter a descriptive title for the study session"
+                  />
+
+                  <Input
+                    isRequired
+                    type="text"
+                    radius="sm"
+                    name="location"
+                    onChange={handleInput}
+                    label="Class"
+                    placeholder="A-01"
+                    endContent={
+                      <SiGoogleclassroom
+                        className="text-2xl text-default-400 pointer-events-none flex-shrink-0"
+                        size={20}
+                      />
+                    }
+                    aria-label="Enter the class or location for the study session"
+                  />
+
+                  <Input
+                    isRequired
+                    type="text"
+                    radius="sm"
+                    name="people"
+                    onChange={handleInput}
+                    label="Teacher & Group"
+                    placeholder="Dr. Leonard, Grp-12, ..."
+                    endContent={
+                      <HiMiniUserGroup
+                        className="text-2xl text-default-400 pointer-events-none flex-shrink-0"
+                        size={20}
+                      />
+                    }
+                    aria-label="Specify the teacher and group involved in the study session"
+                  />
+
+                  <Select
+                    isDisabled
+                    radius="sm"
+                    label="Occurrences"
+                    name="rrule"
+                    selectorIcon={<FaRepeat size={20} />}
+                    classNames={{
+                      selectorIcon:
+                        "text-2xl text-default-400 pointer-events-none flex-shrink-0",
+                    }}
+                    aria-label="Specify recurring schedule for the study session (optional)"
+                  >
+                    <SelectItem key="DAILY">Daily</SelectItem>
+                    <SelectItem key="WEEKLY">Weekly</SelectItem>
+                    <SelectItem key="MONTHLY">Monthly</SelectItem>
+                    <SelectItem key="YEARLY">Yearly</SelectItem>
+                  </Select>
+
+                  <Textarea
+                    type="text"
+                    name="description"
+                    radius="sm"
+                    onChange={handleInput}
+                    label="Description"
+                    placeholder="Anything you want to add"
+                    className="col-span-2"
+                    aria-label="Provide a detailed description for the study session (optional)"
+                  />
+                </ModalBody>
+
+                <ModalFooter>
+                  <Button color="danger" variant="light" onPress={onClose}>
+                    Cancel
+                  </Button>
+                  <Button
+                    isDisabled={error}
+                    color="primary"
+                    onPress={() => {
+                      onClose();
+                      addTask();
+                      setEventsCount((prevCount) => prevCount + 1);
+                    }}
+                    aria-label="Save the newly created study session"
+                  >
+                    Add
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
       </CardBody>
     </Card>
   );
--- src/components/UI/Scheduler.tsx
+++ src/components/UI/Scheduler.tsx
@@ -1,9 +1,10 @@
 "use client";
 /* Utils */
-import { useState, useEffect } from "react";
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
 import { useNextCalendarApp, ScheduleXCalendar } from "@schedule-x/react";
 import { today, getLocalTimeZone } from "@internationalized/date";
 import { useTheme } from "next-themes";
+import { useLocale } from "@react-aria/i18n";
 
 /* Components */
 import { viewWeek } from "@schedule-x/calendar";
@@ -12,57 +13,32 @@ import { viewWeek } from "@schedule-x/calendar";
 import { createDragAndDropPlugin } from "@schedule-x/drag-and-drop";
 import { createResizePlugin } from "@schedule-x/resize";
 import { createEventModalPlugin } from "@schedule-x/event-modal";
-import { createEventRecurrencePlugin } from "@schedule-x/event-recurrence";
 
 /* Styles */
 import "@schedule-x/theme-default/dist/index.css";
 
 /* Types */
-
-type Event = {
-  id: string | number;
-  start: string;
-  end: string;
-  calendarId: string;
-  title?: string;
-  description?: string;
-  location?: string;
-  people?: string[];
-  rrule?: string;
-};
+import type { Event } from "@/app/dashboard/timetable/page";
 
 interface Props {
-  level: string | Object;
+  event: Event;
+  removeAll: boolean;
+  setRemoveAll: Dispatch<SetStateAction<boolean>>;
 }
 
-function Scheduler({ level }: Props) {
-  const [events, setEvents] = useState<Event[]>([
-    {
-      id: 1,
-      calendarId: "school",
-      title: "Coffee with John",
-      start: "2024-05-08 10:00",
-      end: "2024-05-08 10:30",
-    },
-    {
-      id: 2,
-      calendarId: "school",
-      title: "Ski trip",
-      start: "2024-05-08 11:00",
-      end: "2024-05-08 12:00",
-    },
-  ]);
+function Scheduler({ event, removeAll, setRemoveAll }: Props) {
   const [mounted, setMounted] = useState<boolean>(false);
   const { resolvedTheme } = useTheme();
+  const { locale } = useLocale();
 
   const calendar = useNextCalendarApp({
     views: [viewWeek],
     firstDayOfWeek: 0,
     selectedDate: today(getLocalTimeZone()).toString(),
-    locale: "en-UK",
+    locale: locale,
     dayBoundaries: {
       start: "08:00",
-      end: "18:00",
+      end: "16:00",
     },
     weekOptions: {
       gridHeight: 495,
@@ -82,16 +58,39 @@ function Scheduler({ level }: Props) {
         },
       },
     },
-    events: events,
     plugins: [
       createDragAndDropPlugin(30),
       createResizePlugin(30),
       createEventModalPlugin(),
-      createEventRecurrencePlugin(),
     ],
+    callbacks: {
+      onEventUpdate(updatedEvent) {
+        calendar?.events.update(updatedEvent);
+      },
+    },
   });
 
   calendar?.setTheme(resolvedTheme === "light" ? "light" : "dark");
+
+  useEffect(() => {
+    if (!calendar?.events.get(event.id)) {
+      calendar?.events.add(event);
+    } else {
+      calendar?.events.update(event);
+    }
+
+    console.log(calendar?.events.getAll());
+  }, [event, calendar]);
+
+  useEffect(() => {
+    if (removeAll) {
+      calendar?.events
+        .getAll()
+        .map((event) => calendar?.events.remove(event.id));
+
+      setRemoveAll(false);
+    }
+  }, [removeAll, calendar, setRemoveAll]);
 
   useEffect(() => {
     setMounted(true);
